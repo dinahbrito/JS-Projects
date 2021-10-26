8ef17f378b39f39ac893b4288dc21bef234b315e
dinahbrito <dinahbrito@gmail.com>
2021-10-25 18:06:29 -0700

    Added api info and key

--- infinite-scroll/script.js
+++ infinite-scroll/script.js
@@ -1,1 +1,19 @@
-console.log('testing');
+// Unsplash API
+const count = 10;
+const apiKey = 'TithZquHDx-tYGLVWCO7twTMXobZhqfUYIjvWW66RzQ';
+const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Get photos from Unsplash API
+async function getPhotos()
+{
+    try {
+        const response = await fetch(apiURL);
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        // Catch error here
+    }
+}
+
+// On Load
+getPhotos();
